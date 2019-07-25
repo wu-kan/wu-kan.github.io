461876405bf1c3daa328579d1a907dd6b05b8d2f
wu-kan <wu.kan@qq.com>
2019-07-25 14:29:47 +0800

    博文一篇； 无脑cdn加速； 插件JSloader化

--- public/js/prism.js
+++ public/js/prism.js
@@ -2,7 +2,42 @@
   layout: null
 ---
   {% if site.PrismJS.enable %}
+{% for s in site.PrismJS.src %}
+$("<link>").attr({ href: "{{ s.stylesheet }}", rel: "stylesheet" }).appendTo("head");
+{% endfor %}
 {% if site.PrismJS.plugins.line_numbers %}
 $('pre').addClass("line-numbers");
+{% endif %}
+{% if site.PrismJS.plugins.toolbar.show_language.enable %}
+Prism.plugins.toolbar.registerButton('show-language', {
+  text: '{{ site.PrismJS.plugins.toolbar.show_language.text }}', // required
+  onClick: function (env) { // optional
+    alert('{{ site.PrismJS.plugins.toolbar.show_language.alert }}' + env.language);
+  }
+});
+{% endif %}
+{% if site.PrismJS.plugins.toolbar.select_code.enable %}
+Prism.plugins.toolbar.registerButton('select-code', function (env) {
+  var button = document.createElement('button');
+  button.innerHTML = '{{ site.PrismJS.plugins.toolbar.select_code.innerHTML }}';
+  button.addEventListener('click', function () {
+    // Source: http://stackoverflow.com/a/11128179/2757940
+    if (document.body.createTextRange) { // ms
+      var range = document.body.createTextRange();
+      range.moveToElementText(env.element);
+      range.select();
+    } else if (window.getSelection) { // moz, opera, webkit
+      var selection = window.getSelection();
+      var range = document.createRange();
+      range.selectNodeContents(env.element);
+      selection.removeAllRanges();
+      selection.addRange(range);
+    }
+  });
+  return button;
+});
+{% endif %}
+{% if site.PrismJS.plugins.autoloader %}
+Prism.plugins.autoloader.languages_path = '{{ site.PrismJS.plugins.autoloader }}';
 {% endif %}
 {% endif %}
