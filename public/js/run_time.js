c67e576d080d751b9b758bd706db874c218c9aaa
wu-kan <wu.kan@qq.com>
2019-08-28 16:00:50 +0800

    笔记若干&一些调整

---
+++ public/js/run_time.js
@@ -0,0 +1,25 @@
+---
+  layout: null
+---
+  function run_time() {
+    function setzero(i) {
+      if (i < 10) { i = "0" + i };
+      return i;
+    }
+    window.setTimeout("run_time()", 1000);
+    BirthDay = new Date("{{ site.run_time.start_time }}");
+    today = new Date();
+    timeold = (today.getTime() - BirthDay.getTime());
+    sectimeold = timeold / 1000
+    secondsold = Math.floor(sectimeold);
+    msPerDay = 24 * 60 * 60 * 1000
+    e_daysold = timeold / msPerDay
+    daysold = Math.floor(e_daysold);
+    e_hrsold = (e_daysold - daysold) * 24;
+    hrsold = setzero(Math.floor(e_hrsold));
+    e_minsold = (e_hrsold - hrsold) * 60;
+    minsold = setzero(Math.floor((e_hrsold - hrsold) * 60));
+    seconds = setzero(Math.floor((e_minsold - minsold) * 60));
+    document.getElementById('run_time').innerHTML = daysold + "{{ site.run_time.day }}" + hrsold + "{{ site.run_time.hour }}" + minsold + "{{ site.run_time.minute }}" + seconds + "{{ site.run_time.second }}";
+  }
+run_time();
