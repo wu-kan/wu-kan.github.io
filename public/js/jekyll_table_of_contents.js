c6627e9a5f6cf2a77f7f80da8970e8729e4c6a64
wu-kan <wu.kan@qq.com>
2019-09-11 11:45:09 +0800

    界面微调；Github Button

--- public/js/jekyll_table_of_contents.js
+++ public/js/jekyll_table_of_contents.js
@@ -2,7 +2,7 @@
   layout: null
 ---
   document.write('<div id="toc"></div>');
-document.write('<script src="{{ site.jekyll_table_of_contents.src }}"></script>');
+document.write('<script src="{{ site.sidebar.jekyll_table_of_contents.src }}"></script>');
 document.addEventListener("DOMContentLoaded", function () {
   $('#toc').toc();
 });
--- public/simple-jekyll-search/autoloader.js
+++ public/simple-jekyll-search/autoloader.js
@@ -1,9 +1,9 @@
 ---
   layout: null
 ---
-  document.write('<input id="search-input" placeholder="{{ site.simple_jekyll_search.placeholder }}" />');
+  document.write('<input id="search-input" placeholder="{{ site.sidebar.simple_jekyll_search.placeholder }}" />');
 document.write('<div id="results-container"></div>');
-document.write('<script src="{{ site.simple_jekyll_search.src }}"></script>');
+document.write('<script src="{{ site.sidebar.simple_jekyll_search.src }}"></script>');
 function loadStyle(url) {
   var link = document.createElement('link');
   link.rel = 'stylesheet';
@@ -11,10 +11,10 @@ function loadStyle(url) {
   var head = document.getElementsByTagName('head')[0];
   head.appendChild(link);
 }
-loadStyle('{{ site.simple_jekyll_search.stylesheet }}');
+loadStyle('{{ site.sidebar.simple_jekyll_search.stylesheet }}');
 document.addEventListener("DOMContentLoaded", function () {
   SimpleJekyllSearch({
-    json: '{{ site.simple_jekyll_search.json }}',
+    json: '{{ site.sidebar.simple_jekyll_search.json }}',
     searchInput: document.getElementById('search-input'),
     resultsContainer: document.getElementById('results-container')
   });
